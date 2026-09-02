98d69c0d5b9f6b8732fc4b6e78455b67a6e7f64b
Samuel <samuelkttt@gmail.com>
2026-09-01 21:16:36 -0300

    Conteúdo de operadores logicos

---
+++ aulas/OperadoresLogicos.py
@@ -0,0 +1,33 @@
+#OPERADORES
+"""
+    ATRIBUIÇÃO
+    = -> variavel = 10
+
+    ! = NÃO, NOT, CONTRÁRIO...
+
+    COMPARAÇÃO
+    esperar uma resposta de True ou False
+    != -> se for diferente retorna True, se for igual retorna False
+
+    == -> se for diferente retorna False, se for igual retorna True
+
+    > -> se for maior retorna True, se for menor retorna False
+    < -> se for menor retorna False, se for igual retorna True
+
+    >= -> se for maior E igual retorna True, contrario retorna False
+    <= -> se for menor E igual retorna True, contrario retorna False
+
+    PARA MAIS COMPARAÇÕES
+    and -> se todas as comparações forem True, retorna True
+
+    or -> se ao menos uma das comparações for True, retorna True
+    not
+"""
+
+idade = 18
+
+print(idade != 18) # False
+print(idade == 18) # True
+print(idade > 18)  # False
+print(idade < 18)  # False
+print(idade >= 18) # True
